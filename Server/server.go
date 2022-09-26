7c35971f8340e10f53a3bef74d11789dbf6ab739
SandhyaLakshmi0708 <113583277+SandhyaLakshmi0708@users.noreply.github.com>
2022-09-26 11:55:45 +0530

    Add files via upload
    /Server/server.go And /CLietnt/client.go

---
+++ Server/server.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"net"
+	"time"
+)
+
+var port = "0.0.0.0:9001"
+
+func echoMessage(conn net.Conn) {
+
+	r := bufio.NewReader(conn)
+
+	for {
+
+		message, err := r.ReadBytes(byte('\n'))
+
+		switch err {
+
+		case nil:
+
+			break
+
+		case io.EOF:
+
+		default:
+
+			fmt.Println("Error ", err)
+
+		}
+		conn.Write([]byte("From Server :"))
+		conn.Write(message)
+		//time and Ip Address
+		fmt.Println("time stamp And IP Address : ", string(message), time.Now(), conn.RemoteAddr())
+
+	}
+
+}
+
+var NumberOfConnections int = 0
+
+func main() {
+
+	fmt.Println("Start the server ")
+
+	ln, err := net.Listen("tcp", port)
+
+	for {
+
+		conn, _ := ln.Accept()
+
+		//Number of connections
+		NumberOfConnections++
+
+		fmt.Println("Total Connected  Clients are:", NumberOfConnections)
+
+		if err != nil {
+
+			fmt.Println("Error ", err)
+
+			continue
+
+		}
+
+		go echoMessage(conn)
+
+	}
+
+}
